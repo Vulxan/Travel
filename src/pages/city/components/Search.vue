34713852751a9bc5b0e4f5c624564d35cd6b9cc8
Vulxan <32930147+Vulxan@users.noreply.github.com>
2018-08-21 19:48:38 +0800

    Add search

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,64 @@
 <template>
   <div class="search">
-    <input type="text" class="search-input" placeholder="输入城市名或拼音">
+    <div class="search-bar">
+      <input type="text" class="search-input" v-model="keyword" placeholder="输入城市名或拼音">
+    </div>
+    <div class="search-content" ref="search" v-show="!hasNoInput">
+      <ul>
+        <li v-for="item of list" v-text="item.name" :key="item.id" class="search-item border-bottom"></li>
+        <li class="search-item border-bottom" v-show="hasNoData">没有匹配的城市</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    },
+    hasNoInput () {
+      return this.keyword.trim() === ''
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach(val => {
+            if (val.spell.indexOf(this.keyword.trim()) > -1 || val.name.indexOf(this.keyword.trim()) > -1) {
+              result.push(val)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  props: {
+    cities: Object
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.search, {click: true})
+  }
 }
 </script>
 
@@ -16,12 +68,27 @@ export default {
     height .72rem
     padding 0 .1rem
     background-color $bgColor
-    .search-input
-      box-sizing border-box
-      padding 0 .1rem
-      height .62rem
-      width 100%
-      text-align center
-      border-radius .06rem
-      color #666
+    .search-bar
+      .search-input
+        box-sizing border-box
+        padding 0 .1rem
+        height .62rem
+        width 100%
+        text-align center
+        border-radius .06rem
+        color #666
+    .search-content
+      position absolute
+      top 1.56rem
+      left 0
+      right 0
+      bottom 0
+      overflow hidden
+      z-index 1
+      background-color #EEE
+      .search-item
+        line-height .62rem
+        padding .1rem
+        background-color #FFF
+        color #666
 </style>

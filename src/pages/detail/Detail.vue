28302f3cf831da5f44d819086c8b307b69df9f9b
Vulxan <32930147+Vulxan@users.noreply.github.com>
2018-08-23 03:52:25 +0800

    Add header animation

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="detial">
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div class="tempContent"></div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
+
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  .tempContent
+    height 50rem
 </style>
---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="header">
+    <router-link v-show="showAbs" tag="div" to="/" class="header-abs"><div class="iconfont header-abs-back">&#xe624;</div></router-link>
+    <div class="header-fixed" :style="opacityStyle" v-show="!showAbs"><router-link tag="span" to="/" class="iconfont header-fixed-back">&#xe624;</router-link>景点详情</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailHeader',
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    handleScroll () {
+      const top = document.documentElement.scrollTop
+      if (top > 60) {
+        this.showAbs = false
+        let opacity = (top - 60) / 103
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = { opacity }
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@/assets/styles/variables.styl';
+  .header
+    .header-abs
+      position absolute
+      left .2rem
+      top .2rem
+      width .8rem
+      height .8rem
+      border-radius .4rem
+      background-color rgba(0, 0, 0, .8)
+      text-align center
+      line-height .8rem
+      .header-abs-back
+        color #FFF
+        font-size .4rem
+    .header-fixed
+      font-size .3rem
+      overflow hidden
+      height $headerHeight
+      line-height $headerHeight
+      text-align center
+      color #FFF
+      background-color $bgColor
+      position fixed
+      top 0
+      left 0
+      right 0
+      .header-fixed-back
+        position absolute
+        top 0
+        left 0
+        font-size .4rem
+        width .64rem
+        text-align center
+        color #FFF
+</style>
